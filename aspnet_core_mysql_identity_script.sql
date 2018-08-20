ecd372747fbaf483ec934c812ef3c4246b47ada3
John Ambeliotis <jambelnet@users.noreply.github.com>
2018-08-20 09:52:26 +0200

    Add files via upload

---
+++ aspnet_core_mysql_identity_script.sql
@@ -0,0 +1,119 @@
+--
+-- MySql - ASP.NET Core Identity
+--
+
+--
+-- Table structure for table `aspnetroles`
+--
+
+CREATE TABLE IF NOT EXISTS AspNetRoles (
+  `Id` varchar(128) NOT NULL,
+  `Name` varchar(256) NOT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `aspnetuserclaims`
+--
+
+CREATE TABLE IF NOT EXISTS AspNetUserClaims (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `UserId` varchar(128) NOT NULL,
+  `ClaimType` longtext,
+  `ClaimValue` longtext,
+  PRIMARY KEY (`Id`),
+  UNIQUE KEY `Id` (`Id`),
+  KEY `UserId` (`UserId`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `aspnetuserlogins`
+--
+
+CREATE TABLE IF NOT EXISTS AspNetUserLogins (
+  `LoginProvider` varchar(128) NOT NULL,
+  `ProviderKey` varchar(128) NOT NULL,
+  `UserId` varchar(128) NOT NULL,
+  PRIMARY KEY (`LoginProvider`,`ProviderKey`,`UserId`),
+  KEY `ApplicationUser_Logins` (`UserId`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `aspnetuserroles`
+--
+
+CREATE TABLE IF NOT EXISTS AspNetUserRoles (
+  `UserId` varchar(128) NOT NULL,
+  `RoleId` varchar(128) NOT NULL,
+  PRIMARY KEY (`UserId`,`RoleId`),
+  KEY `IdentityRole_Users` (`RoleId`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `aspnetusers`
+--
+
+CREATE TABLE IF NOT EXISTS AspNetUsers (
+  `Id` varchar(128) NOT NULL,
+  `ConcurrencyStamp` varchar(256) DEFAULT NULL,
+  `Email` varchar(256) DEFAULT NULL,
+  `EmailConfirmed` tinyint(1) NOT NULL,
+  `PasswordHash` longtext,
+  `SecurityStamp` longtext,
+  `PhoneNumber` longtext,
+  `PhoneNumberConfirmed` tinyint(1) NOT NULL,
+  `TwoFactorEnabled` tinyint(1) NOT NULL,
+  `LockoutEndDateUtc` datetime DEFAULT NULL,
+  `LockoutEnabled` tinyint(1) NOT NULL,
+  `AccessFailedCount` int(11) NOT NULL,
+  `UserName` varchar(256) NOT NULL,
+  `LockoutEnd` datetime DEFAULT NULL,
+  `NormalizedEmail` varchar(256) DEFAULT NULL,
+  `NormalizedUserName` varchar(256) DEFAULT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `aspnetusertokens`
+--
+
+CREATE TABLE IF NOT EXISTS AspNetUserTokens (
+`UserId` varchar(128) NOT NULL,
+`LoginProvider` varchar(127) NOT NULL,
+Name varchar(127) NOT NULL,
+Value longtext,
+CONSTRAINT PK_AspNetUserTokens PRIMARY KEY (UserId, LoginProvider, Name)
+);
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `aspnetuserclaims`
+--
+ALTER TABLE AspNetUserClaims
+  ADD CONSTRAINT `ApplicationUser_Claims` FOREIGN KEY (`UserId`) REFERENCES `AspNetUsers` (`Id`) ON DELETE CASCADE ON UPDATE NO ACTION;
+
+--
+-- Constraints for table `aspnetuserlogins`
+--
+ALTER TABLE AspNetUserLogins
+  ADD CONSTRAINT `ApplicationUser_Logins` FOREIGN KEY (`UserId`) REFERENCES `AspNetUsers` (`Id`) ON DELETE CASCADE ON UPDATE NO ACTION;
+
+--
+-- Constraints for table `aspnetuserroles`
+--
+ALTER TABLE AspNetUserRoles
+  ADD CONSTRAINT `ApplicationUser_Roles` FOREIGN KEY (`UserId`) REFERENCES `AspNetUsers` (`Id`) ON DELETE CASCADE ON UPDATE NO ACTION,
+  ADD CONSTRAINT `IdentityRole_Users` FOREIGN KEY (`RoleId`) REFERENCES `AspNetRoles` (`Id`) ON DELETE CASCADE ON UPDATE NO ACTION;
